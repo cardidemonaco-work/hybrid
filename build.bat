08c731cb775c8e7937de95be6867d8ffebdf52ca
cardidemonaco-work <cardi.demonaco@dynatrace.com>
2016-01-25 15:33:11 -0500

    Changed users on my work workstation to commit this. Used to be connected with my personal account.

--- build.bat
+++ build.bat
@@ -1,3 +1,6 @@
+rem MUST MAKE SURE YOU ARE IN THE CORRECT DIRECTORY BEFORE RUNNING THIS
+rem Correct directory is \PROJECT\BRANCH\
+
 rem Apache Cordova project
 rem C:\Projects\hybrid\trunk\hybrid
 
@@ -7,6 +10,25 @@ rem C:\Projects\hybrid\trunk\hybrid\www
 rem Cordova
 rem C:\Projects\hybrid\trunk\node_modules\cordova\bin\cordova build
 
-cd "hybrid"
-..\node_modules\cordova\bin\cordova build android --release
-cd ..\..\
+rem ===
+
+rem Apache Cordova build
+cd "hybrid" 
+
+rem Create Android Debug build
+call ..\node_modules\cordova\bin\cordova build android 
+
+rem Create Android Production build
+rem ..\node_modules\cordova\bin\cordova build android --release
+
+cd ..\
+
+rem Dynatrace Instrumentation... send to 6.2 Server cd-dt-0602-win
+cd "dynatrace-mobile-adk-6.2.5.1006\Android\auto-instrumentor" 
+
+rem Create Dynatrace-instrumented Debug build
+call instrument.cmd apk="..\..\..\hybrid\platforms\android\build\outputs\apk\android-debug.apk" prop="APK-Instr.properties" 
+
+rem Complete!!!
+rem Final APK here: C:\Projects\hybrid\trunk\hybrid\platforms\android\build\outputs\apk\android-debug\dist
+rem Final APK here relative: ..\..\..\hybrid\platforms\android\build\outputs\apk\android-debug\dist
